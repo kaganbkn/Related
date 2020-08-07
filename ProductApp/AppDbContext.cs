613c6f38e75be87f66865f35c6084f3565c8f6dd
Kağan Beken <kagan.beken@sestek.com>
2020-08-07 14:50:04 +0300

    Add project files.

---
+++ ProductApp/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProductApp.Entities;
+
+namespace ProductApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    ProductId = Guid.NewGuid(),
+                    Name = "Jean",
+                    Price = 10.3,
+                    Tags = new List<Tag>
+                    {
+                        new Tag
+                        {
+                            TagId = Guid.NewGuid(),
+                            Value = "blue"
+                        },
+
+                        new Tag
+                        {
+                            TagId = Guid.NewGuid(),
+                            Value = "32"
+                        }
+                    }
+                });
+        }
+    }
+}
---
+++ ProductApp/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductApp.Entities;
+
+namespace ProductApp
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
